212f8d4b3e10b06c9f57ecad9438ce210fc2b864
Michael Wood-Vasey <wmwv@pitt.edu>
2017-11-27 09:57:35 -0500

    Use generalized benchmarkDistance for N=1 tests.

--- benchmark_test.go
+++ benchmark_test.go
@@ -21,27 +21,14 @@ func BenchmarkEinv(b *testing.B) {
 	}
 }
 
-func BenchmarkComovingDistance(b *testing.B) {
+// benchmarkDistance is a helper function to be called by specific benchmarks
+func benchmarkDistance(distFunc string, b *testing.B) {
 	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
 	z := 1.0
-	for i := 0; i < b.N; i++ {
-		cos.ComovingTransverseDistance(z)
-	}
-}
 
-func BenchmarkComovingTransverseDistance(b *testing.B) {
-	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-	z := 1.0
+	funcToTest := reflect.ValueOf(&cos).MethodByName(distFunc)
 	for i := 0; i < b.N; i++ {
-		cos.ComovingTransverseDistance(z)
-	}
-}
-
-func BenchmarkLuminosityDistance(b *testing.B) {
-	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-	z := 1.0
-	for i := 0; i < b.N; i++ {
-		cos.LuminosityDistance(z)
+		funcToTest.Call([]reflect.Value{reflect.ValueOf(z)})
 	}
 }
 
@@ -60,10 +47,22 @@ func benchmarkNdistance(n int, distFunc string, b *testing.B) {
 	}
 }
 
+func BenchmarkComovingDistanceDistance(b *testing.B) {
+	benchmarkDistance("ComovingDistance", b)
+}
+
+func BenchmarkComovingTransverseDistanceDistance(b *testing.B) {
+	benchmarkDistance("ComovingTransverseDistance", b)
+}
+
+func BenchmarkLuminosityDistance(b *testing.B) {
+	benchmarkDistance("LuminosityDistance", b)
+}
+
 func BenchmarkNComovingDistance(b *testing.B) {
-    benchmarkNdistance(10000, "ComovingDistance", b)
+	benchmarkNdistance(10000, "ComovingDistance", b)
 }
 
 func BenchmarkNLuminosityDistance(b *testing.B) {
-    benchmarkNdistance(10000, "LuminosityDistance", b)
+	benchmarkNdistance(10000, "LuminosityDistance", b)
 }
