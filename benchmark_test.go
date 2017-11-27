560a6971151fc48f44bfc2a0d1fb6884d1cfa9cf
Michael Wood-Vasey <wmwv@pitt.edu>
2017-11-27 09:16:08 -0500

    Add N loop ComovingDistance benchmark.

--- benchmark_test.go
+++ benchmark_test.go
@@ -44,9 +44,23 @@ func BenchmarkLuminosityDistance(b *testing.B) {
 	}
 }
 
+func BenchmarkNComovingDistance(b *testing.B) {
+    cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
+    const n = 10000
+    var z float64
+    z_max := 1.0
+    step := z_max / n
+    for i := 0; i < b.N; i++ {
+        for j := 0; j < n; j++ {
+            z = 0.001 + step*float64(j)
+            cos.ComovingDistance(z)
+        }
+    }
+}
+
 func BenchmarkNLuminosityDistance(b *testing.B) {
 	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-	const n = 10001
+	const n = 10000
 	var z float64
 	z_max := 1.0
 	step := z_max / n
