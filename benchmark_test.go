1ba576814dd1a363854ef32cc927c3b26196f466
Michael Wood-Vasey <wmwv@pitt.edu>
2017-11-27 09:53:45 -0500

    Adopt benchmarkNdistance for Comoving, Lumdist tests.

--- benchmark_test.go
+++ benchmark_test.go
@@ -48,7 +48,6 @@ func BenchmarkLuminosityDistance(b *testing.B) {
 // benchmarkNdistance is a helper function to be called by specific benchmarks
 func benchmarkNdistance(n int, distFunc string, b *testing.B) {
 	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-//	var funcToTest func(float64) float64
 	funcToTest := reflect.ValueOf(&cos).MethodByName(distFunc)
 	var z float64
 	z_max := 1.0
@@ -62,29 +61,9 @@ func benchmarkNdistance(n int, distFunc string, b *testing.B) {
 }
 
 func BenchmarkNComovingDistance(b *testing.B) {
-	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-	const n = 10000
-	var z float64
-	z_max := 1.0
-	step := z_max / n
-	for i := 0; i < b.N; i++ {
-		for j := 0; j < n; j++ {
-			z = 0.001 + step*float64(j)
-			cos.ComovingDistance(z)
-		}
-	}
+    benchmarkNdistance(10000, "ComovingDistance", b)
 }
 
 func BenchmarkNLuminosityDistance(b *testing.B) {
-	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-	const n = 10000
-	var z float64
-	z_max := 1.0
-	step := z_max / n
-	for i := 0; i < b.N; i++ {
-		for j := 0; j < n; j++ {
-			z = 0.001 + step*float64(j)
-			cos.LuminosityDistance(z)
-		}
-	}
+    benchmarkNdistance(10000, "LuminosityDistance", b)
 }
