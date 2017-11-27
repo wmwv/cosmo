90b4f3808b521aac3bef47c6849f0d43a5cff452
Michael Wood-Vasey <wmwv@pitt.edu>
2017-11-27 09:47:38 -0500

    Start generic benchmark distance testing function.

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,7 @@
 package cosmo
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -44,18 +45,34 @@ func BenchmarkLuminosityDistance(b *testing.B) {
 	}
 }
 
+// benchmarkNdistance is a helper function to be called by specific benchmarks
+func benchmarkNdistance(n int, distFunc string, b *testing.B) {
+	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
+//	var funcToTest func(float64) float64
+	funcToTest := reflect.ValueOf(&cos).MethodByName(distFunc)
+	var z float64
+	z_max := 1.0
+	step := z_max / float64(n)
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < n; j++ {
+			z = 0.001 + step*float64(j)
+			funcToTest.Call([]reflect.Value{reflect.ValueOf(z)})
+		}
+	}
+}
+
 func BenchmarkNComovingDistance(b *testing.B) {
-    cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
-    const n = 10000
-    var z float64
-    z_max := 1.0
-    step := z_max / n
-    for i := 0; i < b.N; i++ {
-        for j := 0; j < n; j++ {
-            z = 0.001 + step*float64(j)
-            cos.ComovingDistance(z)
-        }
-    }
+	cos := Cosmology{Om0: 0.27, Ol0: 0.73, Ok0: 0., H0: 70, w0: -1.0, Tcmb0: 0.}
+	const n = 10000
+	var z float64
+	z_max := 1.0
+	step := z_max / n
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < n; j++ {
+			z = 0.001 + step*float64(j)
+			cos.ComovingDistance(z)
+		}
+	}
 }
 
 func BenchmarkNLuminosityDistance(b *testing.B) {
