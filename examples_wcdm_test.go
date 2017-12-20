e59eaf03d89e1a12db3cb885fa16c47afa805329
Michael Wood-Vasey <wmwv@pitt.edu>
2017-12-20 15:56:52 -0500

    Merge Example functions into main WCDM doc.

--- examples_wcdm_test.go
+++ examples_wcdm_test.go
@@ -4,47 +4,30 @@ import (
 	"fmt"
 )
 
-func ExampleWCDM_DistanceModulus() {
-	var z_vec, obs_vec []float64
+func ExampleWCDM() {
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1.2, H0: 70, Tcmb0: 0.}
 
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	obs_vec = make([]float64, len(z_vec))
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	distmod_vec := make([]float64, len(z_vec))
+	lumdist_vec := make([]float64, len(z_vec))
+	angdist_vec := make([]float64, len(z_vec))
 	for i, z := range z_vec {
-		obs_vec[i] = cos.DistanceModulus(z)
+		distmod_vec[i] = cos.DistanceModulus(z)
+		lumdist_vec[i] = cos.LuminosityDistance(z)
+		angdist_vec[i] = cos.AngularDiameterDistance(z)
 	}
 
-	fmt.Println(obs_vec)
+	fmt.Println("Distance Modulus [mag]")
+	fmt.Println(distmod_vec)
+	fmt.Println("Luminosity Distance [Mpc]")
+	fmt.Println(lumdist_vec)
+	fmt.Println("Angular Diameter Distance [Mpc]")
+	fmt.Println(angdist_vec)
 	// Output:
+	// Distance Modulus [mag]
 	// [42.26118542154089 44.10023765554372 45.95719725271018 47.026111928689645]
-}
-
-func ExampleWCDM_LuminosityDistance() {
-	var z_vec, obs_vec []float64
-	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
-
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	obs_vec = make([]float64, len(z_vec))
-	for i, z := range z_vec {
-		obs_vec[i] = cos.LuminosityDistance(z)
-	}
-
-	fmt.Println(obs_vec)
-	// Output:
+	// Luminosity Distance [Mpc]
 	// [2832.938093900105 6607.65761177494 15539.58622322811 25422.74174518986]
-}
-
-func ExampleWCDM_AngularDiameterDistance() {
-	var z_vec, obs_vec []float64
-	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
-
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	obs_vec = make([]float64, len(z_vec))
-	for i, z := range z_vec {
-		obs_vec[i] = cos.AngularDiameterDistance(z)
-	}
-
-	fmt.Println(obs_vec)
-	// Output:
+	// Angular Diameter Distance [Mpc]
 	// [1259.0835972889354 1651.914402943735 1726.6206914697902 1588.9213590743661]
 }
