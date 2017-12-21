01f4262aaf97c446d2245229315d0228b785dddd
Michael Wood-Vasey <wmwv@pitt.edu>
2017-12-21 10:19:29 -0500

    Adopt lambdacdm_test to runTest[s]

--- lambdacdm_test.go
+++ lambdacdm_test.go
@@ -21,7 +21,7 @@ func TestLambdaCDMCosmologyInterface(t *testing.T) {
 // TestE* tests that basic calculation of E
 //   https://github.com/astropy/astropy/blob/master/astropy/cosmology/tests/test_cosmology.py
 func TestLambdaCDMELcdm(t *testing.T) {
-	var exp, obs, tol float64
+	var z, exp, tol float64
 	cos := LambdaCDM{Om0: 0.27, Ol0: 0.73, H0: 70, Tcmb0: 0.}
 
 	// Check value of E(z=1.0)
@@ -30,255 +30,152 @@ func TestLambdaCDMELcdm(t *testing.T) {
 	//   sqrt(0.27*(1+1.0)**3 + 0.0 * (1+1.0)**2 + 0.73)
 	//   sqrt(0.27*8 + 0 + 0.73)
 	//   sqrt(2.89)
+	z = 1.0
 	exp = 1.7
-	obs = cos.E(1.0)
 	tol = 1e-9
-	if !floats.EqualWithinAbs(obs, exp, tol) {
-		t.Errorf("Failed flat LCDM E(z) test.  Expected %f, return %f",
-			exp, obs)
-	}
+	runTest(cos.E, z, exp, tol, t, 0)
 
 	exp = 1 / 1.7
-	obs = cos.Einv(1.0)
-	if !floats.EqualWithinAbs(obs, exp, tol) {
-		t.Errorf("Failed flat LCDM Einv(z) test.  Expected %f, return %f",
-			exp, obs)
-	}
+	runTest(cos.Einv, z, exp, tol, t, 0)
 }
 
 func TestLambdaCDMDistanceModulus(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-8
+	tol := 1e-8
 	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
 	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{42.26118542, 44.10023766, 45.95719725, 47.02611193}
-	for i, z := range z_vec {
-		obs = cos.DistanceModulus(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM luminosity distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTests(cos.DistanceModulus, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLuminosityDistanceFlat(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	// Calculated via
 	//   from astropy.cosmology import LambdaCDM
 	//   z = np.asarray([0.5, 1.0, 2.0, 3.0])
 	//   LambdaCDM(70, 0.3, 0.7).luminosity_distance(z)
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{2832.9380939, 6607.65761177, 15539.58622323, 25422.74174519}
-	for i, z := range z_vec {
-		obs = cos.LuminosityDistance(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM luminosity distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTests(cos.LuminosityDistance, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLuminosityDistanceNonflat(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.6, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	// Calculated via
 	//   from astropy.cosmology import LambdaCDM
 	//   z = np.asarray([0.5, 1.0, 2.0, 3.0])
 	//   LambdaCDM(70, 0.3, 0.6).luminosity_distance(z)
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{2787.51504671, 6479.83450953, 15347.21516211, 25369.7240234}
-	for i, z := range z_vec {
-		obs = cos.LuminosityDistance(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed nonflat LCDM luminosity distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTests(cos.LuminosityDistance, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAngularDiameterDistance(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
 	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{1259.08359729, 1651.91440294, 1726.62069147, 1588.92135907}
-	for i, z := range z_vec {
-		obs = cos.AngularDiameterDistance(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM angular diameter distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTests(cos.AngularDiameterDistance, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMComovingTransverseDistance(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
 	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
-	for i, z := range z_vec {
-		obs = cos.ComovingTransverseDistance(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM comoving transverse distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTests(cos.ComovingTransverseDistance, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMComovingDistanceZ1Z2Integrate(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
 	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
-	for i, z := range z_vec {
-		obs = cos.ComovingDistanceZ1Z2Integrate(0, z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM comoving distance elliptic test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTestsZ0Z2(cos.ComovingDistanceZ1Z2Integrate, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMComovingDistanceZ1Z2Elliptic(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
 	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
-	for i, z := range z_vec {
-		obs = cos.ComovingDistanceZ1Z2Elliptic(0, z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM comoving distance elliptic test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
-	}
+	runTestsZ0Z2(cos.ComovingDistanceZ1Z2Elliptic, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLookbackTime(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.FlatLambdaCDM(70, 0.3).lookback_time
 	exp_vec = []float64{5.04063793, 7.715337, 10.24035689, 11.35445676}
-	for i, z := range z_vec {
-		obs = cos.LookbackTime(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM lookback time test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.LookbackTime, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLookbackTimeIntegrate(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.FlatLambdaCDM(70, 0.3).lookback_time
 	exp_vec = []float64{5.04063793, 7.715337, 10.24035689, 11.35445676}
-	for i, z := range z_vec {
-		obs = cos.LookbackTimeIntegrate(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM lookback time integrate test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.LookbackTimeIntegrate, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLookbackTimeOM(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0., H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.FlatLambdaCDM(70, 0.3).lookback_time
 	exp_vec = []float64{4.51471693, 6.62532254, 8.57486509, 9.45923582}
-	for i, z := range z_vec {
-		obs = cos.LookbackTimeOM(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed (OM, OL)=(0.3, 0) lookback time test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.LookbackTimeOM, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMLookbackTimeOL(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0., Ol0: 0.5, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.FlatLambdaCDM(70, 0.3).lookback_time
 	exp_vec = []float64{5.0616361, 7.90494991, 10.94241739, 12.52244605}
-	for i, z := range z_vec {
-		obs = cos.LookbackTimeOL(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed (OM, OL)=(0, 0.5) lookback time test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.LookbackTimeOL, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAge(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.6, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via
@@ -287,22 +184,14 @@ func TestLambdaCDMAge(t *testing.T) {
 	//   astropy.cosmology.LambdaCDM(70, 0.3, 0.6).age(z)
 	exp_vec = []float64{8.11137578, 5.54558439, 3.13456008, 2.06445301}
 
-	for i, z := range z_vec {
-		obs = cos.Age(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed (OM, OL) = (%.2f, %.2f) LCDM age test."+
-				"  Expected %f, return %f",
-				cos.Om0, cos.Ol0, exp_vec[i], obs)
-		}
-	}
+	runTests(cos.Age, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAgeFlatLCDM(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 
 	// Calculated via
@@ -311,22 +200,14 @@ func TestLambdaCDMAgeFlatLCDM(t *testing.T) {
 	//   astropy.cosmology.FlatLambdaCDM(70, 0.3).age(z)
 	exp_vec = []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
 
-	for i, z := range z_vec {
-		obs = cos.Age(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM age test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.Age, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAgeIntegrate(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0.7, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 
 	// Calculated via
@@ -335,81 +216,47 @@ func TestLambdaCDMAgeIntegrate(t *testing.T) {
 	//   astropy.cosmology.FlatLambdaCDM(70, 0.3).age(z)
 	exp_vec = []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
 
-	for i, z := range z_vec {
-		obs = cos.AgeIntegrate(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed flat LCDM ageIntegrate test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.AgeIntegrate, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAgeOM(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
-	var ageIntegrate float64
 	cos := LambdaCDM{Om0: 0.3, Ol0: 0., H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via
 	//   import astropy.cosmology
 	//   z = [0.5, 1.0, 2.0, 3.0]
 	//   astropy.cosmology.LambdaCDM(70, 0.3, 0.).age(z)
 	exp_vec = []float64{6.78287955, 4.67227393, 2.72273139, 1.83836065}
-	for i, z := range z_vec {
-		obs = cos.AgeOM(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed (OM, OL)=(0.3, 0) age test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-		ageIntegrate = cos.AgeIntegrate(z)
-		if !floats.EqualWithinAbs(obs, ageIntegrate, tol) {
-			t.Errorf("Failed (OM, OL)=(0.3, 0) comparison "+
-				"between AgeOM (%f) and AgeIntegrate (%f).",
-				obs, ageIntegrate)
-		}
-	}
+	runTests(cos.AgeOM, z_vec, exp_vec, tol, t)
+	runTests(cos.AgeIntegrate, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMAgeOL(t *testing.T) {
 	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := LambdaCDM{Om0: 0., Ol0: 0.5, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.FlatLambdaCDM(70, 0.3).lookback_time
 	exp_vec = []float64{12.34935796, 9.50604415, 6.46857667, 4.88854801}
-	for i, z := range z_vec {
-		obs = cos.AgeOL(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed (OM, OL)=(0, 0.5) age test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-	}
+	runTests(cos.AgeOL, z_vec, exp_vec, tol, t)
 }
 
 // Analytic case of Omega_Lambda = 0
 func TestLambdaCDMEOm(t *testing.T) {
-	var z_vec []float64
-	var obs, exp, tol float64
+	var z_vec, exp_vec []float64
 	cos := LambdaCDM{Om0: 1.0, Ol0: 0., H0: 70, Tcmb0: 0.}
-	tol = 1e-9
+	tol := 1e-9
 	z_vec = []float64{1.0, 10.0, 500.0, 1000.0}
 	hubbleDistance := SpeedOfLightKmS / cos.H0
-	for _, z := range z_vec {
-		exp = 2.0 * hubbleDistance * (1 - math.Sqrt(1/(1+z)))
-		obs = cos.ComovingDistance(z)
-		if !floats.EqualWithinAbs(obs, exp, tol) {
-			t.Errorf("Failed OM, OL = (1, 0) analytic comoving distance test."+
-				"  Expected %f, return %f",
-				exp, obs)
-		}
+	exp_vec = make([]float64, len(z_vec))
+	for i, z := range z_vec {
+		exp_vec[i] = 2.0 * hubbleDistance * (1 - math.Sqrt(1/(1+z)))
 	}
+	runTests(cos.ComovingDistance, z_vec, exp_vec, tol, t)
 }
 
 func TestLambdaCDMEvecLcdm(t *testing.T) {
