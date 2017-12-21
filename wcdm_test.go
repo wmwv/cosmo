1c028c4293bfa9ea455dbf8d494695ce4f704c15
Michael Wood-Vasey <wmwv@pitt.edu>
2017-12-21 07:53:40 -0500

    Reduce boilerplate with test function.  Define more variables in place.

--- wcdm_test.go
+++ wcdm_test.go
@@ -21,7 +21,6 @@ func TestWCDMCosmologyInterface(t *testing.T) {
 // TestE* tests that basic calculation of E
 //   https://github.com/astropy/astropy/blob/master/astropy/cosmology/tests/test_cosmology.py
 func TestWCDMELcdm(t *testing.T) {
-	var exp, obs, tol float64
 	cos := WCDM{Om0: 0.27, Ol0: 0.73, W0: -1, H0: 70, Tcmb0: 0.}
 
 	// Check value of E(z=1.0)
@@ -30,9 +29,9 @@ func TestWCDMELcdm(t *testing.T) {
 	//   sqrt(0.27*(1+1.0)**3 + 0.0 * (1+1.0)**2 + 0.73)
 	//   sqrt(0.27*8 + 0 + 0.73)
 	//   sqrt(2.89)
-	exp = 1.7
-	obs = cos.E(1.0)
-	tol = 1e-9
+	exp := 1.7
+	obs := cos.E(1.0)
+	tol := 1e-9
 	if !floats.EqualWithinAbs(obs, exp, tol) {
 		t.Errorf("Failed flat wCDM E(z) test.  Expected %f, return %f",
 			exp, obs)
@@ -47,20 +46,19 @@ func TestWCDMELcdm(t *testing.T) {
 }
 
 func TestWCDMDistanceModulus(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1.2, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-8
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
+	tol := 1e-8
+	//  z_vec := []float64{0.2, 0.4, 0.9, 1.2}
+	//  exp_vec := []float64{971.667, 2141.67, 5685.96, 8107.41}
 	// Calculated via
 	//   from astropy.cosmology import wCDM
 	//   z = np.asarray([0.5, 1.0, 2.0, 3.0])
 	//   wCDM(70, 0.3, 0.7, -1.2).distmod(z)
 
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{42.32710911, 44.17957201, 46.03118144, 47.09228735}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{42.32710911, 44.17957201, 46.03118144, 47.09228735}
 	for i, z := range z_vec {
 		obs = cos.DistanceModulus(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -73,15 +71,12 @@ func TestWCDMDistanceModulus(t *testing.T) {
 }
 
 func TestWCDMLuminosityDistanceFlatCDM(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{2832.9380939, 6607.65761177, 15539.58622323, 25422.74174519}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{2832.9380939, 6607.65761177, 15539.58622323, 25422.74174519}
 	for i, z := range z_vec {
 		obs = cos.LuminosityDistance(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -94,17 +89,16 @@ func TestWCDMLuminosityDistanceFlatCDM(t *testing.T) {
 }
 
 func TestWCDMLuminosityDistanceFlat(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1.1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	// Calculated via
 	//   from astropy.cosmology import LambdaCDM
 	//   z = np.asarray([0.5, 1.0, 2.0, 3.0])
 	//   wCDM(70, 0.3, 0.7, -1.1).luminosity_distance(z)
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{2877.10314183, 6734.38177991, 15823.59621899, 25841.56448508}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{2877.10314183, 6734.38177991, 15823.59621899, 25841.56448508}
 	for i, z := range z_vec {
 		obs = cos.LuminosityDistance(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -116,39 +110,37 @@ func TestWCDMLuminosityDistanceFlat(t *testing.T) {
 	}
 }
 
+func test(cos_func func(float64) float64, z, exp, tol float64, test_description string, t *testing.T) {
+	obs := cos_func(z)
+	if !floats.EqualWithinAbs(obs, exp, tol) {
+		t.Errorf("Failed %s test."+"  Expected %f, return %f", exp, obs, test_description)
+	}
+
+}
+
 func TestWCDMLuminosityDistanceNonflat(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.6, W0: -0.8, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 	// Calculated via
 	//   from astropy.cosmology import LambdaCDM
 	//   z = np.asarray([0.5, 1.0, 2.0, 3.0])
 	//   wCDM(70, 0.3, 0.6, -0.8).luminosity_distance(z)
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{2713.4660301, 6257.24866642, 14794.59911147, 24496.30592953}
+	test_description := "nonLambda, nonflat wCDM luminosity distance"
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{2713.4660301, 6257.24866642, 14794.59911147, 24496.30592953}
 	for i, z := range z_vec {
-		obs = cos.LuminosityDistance(z)
-		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
-			t.Errorf("Failed nonLambda, nonflat wCDM luminosity distance test."+
-				"  Expected %f, return %f",
-				exp_vec[i], obs)
-		}
-
+		test(cos.LuminosityDistance, z, exp_vec[i], tol, test_description, t)
 	}
 }
 
 func TestWCDMAngularDiameterDistance(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{1259.08359729, 1651.91440294, 1726.62069147, 1588.92135907}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{1259.08359729, 1651.91440294, 1726.62069147, 1588.92135907}
 	for i, z := range z_vec {
 		obs = cos.AngularDiameterDistance(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -161,15 +153,14 @@ func TestWCDMAngularDiameterDistance(t *testing.T) {
 }
 
 func TestWCDMComovingTransverseDistance(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
+	tol := 1e-6
+	//  z_vec := []float64{0.2, 0.4, 0.9, 1.2}
+	//  exp_vec := []float64{971.667, 2141.67, 5685.96, 8107.41}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
 	for i, z := range z_vec {
 		obs = cos.ComovingTransverseDistance(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -182,15 +173,14 @@ func TestWCDMComovingTransverseDistance(t *testing.T) {
 }
 
 func TestWCDMComovingDistanceZ1Z2Integrate(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
+	tol := 1e-6
+	//  z_vec := []float64{0.2, 0.4, 0.9, 1.2}
+	//  exp_vec := []float64{971.667, 2141.67, 5685.96, 8107.41}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
 	for i, z := range z_vec {
 		obs = cos.ComovingDistanceZ1Z2Integrate(0, z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -203,15 +193,14 @@ func TestWCDMComovingDistanceZ1Z2Integrate(t *testing.T) {
 }
 
 func TestWCDMComovingDistanceZ1Z2Elliptic(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	//  z_vec = []float64{0.2, 0.4, 0.9, 1.2}
-	//  exp_vec = []float64{971.667, 2141.67, 5685.96, 8107.41}
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
-	exp_vec = []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
+	tol := 1e-6
+	//  z_vec := []float64{0.2, 0.4, 0.9, 1.2}
+	//  exp_vec := []float64{971.667, 2141.67, 5685.96, 8107.41}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
+	exp_vec := []float64{1888.62539593, 3303.82880589, 5179.86207441, 6355.6854363}
 	for i, z := range z_vec {
 		obs = cos.ComovingDistanceZ1Z2Elliptic(0, z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -224,14 +213,13 @@ func TestWCDMComovingDistanceZ1Z2Elliptic(t *testing.T) {
 }
 
 func TestWCDMLookbackTime(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1.2, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.wCDM(70, 0.3, 0.7, w0=-1.2).lookback_time
-	exp_vec = []float64{5.18796426, 7.98542226, 10.58842012, 11.71902479}
+	exp_vec := []float64{5.18796426, 7.98542226, 10.58842012, 11.71902479}
 	for i, z := range z_vec {
 		obs = cos.LookbackTime(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -243,14 +231,13 @@ func TestWCDMLookbackTime(t *testing.T) {
 }
 
 func TestWCDMLookbackTimeIntegrate(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1.1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.wCDM(70, 0.3, 0.7, w0=-1.1).lookback_time
-	exp_vec = []float64{5.11509518, 7.85406053, 10.42213038, 11.54588106}
+	exp_vec := []float64{5.11509518, 7.85406053, 10.42213038, 11.54588106}
 	for i, z := range z_vec {
 		obs = cos.LookbackTimeIntegrate(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -262,14 +249,13 @@ func TestWCDMLookbackTimeIntegrate(t *testing.T) {
 }
 
 func TestWCDMLookbackTimeOM(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0., W0: -0.9, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.wCDM(70, 0.3, 0.7, w0=-0.9).lookback_time
-	exp_vec = []float64{4.51471693, 6.62532254, 8.57486509, 9.45923582}
+	exp_vec := []float64{4.51471693, 6.62532254, 8.57486509, 9.45923582}
 	for i, z := range z_vec {
 		obs = cos.LookbackTimeOM(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -281,14 +267,13 @@ func TestWCDMLookbackTimeOM(t *testing.T) {
 }
 
 func TestWCDMLookbackTimeOL(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0., Ol0: 0.5, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via astropy.cosmology.wCDM(70, 0.3).lookback_time
-	exp_vec = []float64{5.0616361, 7.90494991, 10.94241739, 12.52244605}
+	exp_vec := []float64{5.0616361, 7.90494991, 10.94241739, 12.52244605}
 	for i, z := range z_vec {
 		obs = cos.LookbackTimeOL(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -300,18 +285,17 @@ func TestWCDMLookbackTimeOL(t *testing.T) {
 }
 
 func TestWCDMAge(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.6, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
+	tol := 1e-6
 
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via
 	//   import astropy.cosmology
 	//   z = [0.5, 1.0, 2.0, 3.0]
 	//   astropy.cosmology.WCDM(70, 0.3, 0.6).age(z)
-	exp_vec = []float64{8.11137578, 5.54558439, 3.13456008, 2.06445301}
+	exp_vec := []float64{8.11137578, 5.54558439, 3.13456008, 2.06445301}
 
 	for i, z := range z_vec {
 		obs = cos.Age(z)
@@ -324,18 +308,17 @@ func TestWCDMAge(t *testing.T) {
 }
 
 func TestWCDMAgeFlatLCDM(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 
 	// Calculated via
 	//   import astropy.cosmology
 	//   z = [0.5, 1.0, 2.0, 3.0]
 	//   astropy.cosmology.wCDM(70, 0.3).age(z)
-	exp_vec = []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
+	exp_vec := []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
 
 	for i, z := range z_vec {
 		obs = cos.Age(z)
@@ -348,18 +331,17 @@ func TestWCDMAgeFlatLCDM(t *testing.T) {
 }
 
 func TestWCDMAgeIntegrate(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	cos := WCDM{Om0: 0.3, Ol0: 0.7, W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 
 	// Calculated via
 	//   import astropy.cosmology
 	//   z = [0.5, 1.0, 2.0, 3.0]
 	//   astropy.cosmology.wCDM(70, 0.3).age(z)
-	exp_vec = []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
+	exp_vec := []float64{8.42634602, 5.75164694, 3.22662706, 2.11252719}
 
 	for i, z := range z_vec {
 		obs = cos.AgeIntegrate(z)
@@ -372,18 +354,17 @@ func TestWCDMAgeIntegrate(t *testing.T) {
 }
 
 func TestWCDMAgeOM(t *testing.T) {
-	var z_vec, exp_vec []float64
-	var obs, tol float64
+	var obs float64
 	var ageIntegrate float64
 	cos := WCDM{Om0: 0.3, Ol0: 0., W0: -1, H0: 70, Tcmb0: 0.}
 
-	tol = 1e-6
-	z_vec = []float64{0.5, 1.0, 2.0, 3.0}
+	tol := 1e-6
+	z_vec := []float64{0.5, 1.0, 2.0, 3.0}
 	// Calculated via
 	//   import astropy.cosmology
 	//   z = [0.5, 1.0, 2.0, 3.0]
 	//   astropy.cosmology.WCDM(70, 0.3, 0.).age(z)
-	exp_vec = []float64{6.78287955, 4.67227393, 2.72273139, 1.83836065}
+	exp_vec := []float64{6.78287955, 4.67227393, 2.72273139, 1.83836065}
 	for i, z := range z_vec {
 		obs = cos.AgeOM(z)
 		if !floats.EqualWithinAbs(obs, exp_vec[i], tol) {
@@ -402,11 +383,11 @@ func TestWCDMAgeOM(t *testing.T) {
 
 // Analytic case of Omega_Lambda = 0
 func TestWCDMEOm(t *testing.T) {
-	var z_vec []float64
-	var obs, exp, tol float64
+	var obs, exp float64
 	cos := WCDM{Om0: 1.0, Ol0: 0., W0: -1, H0: 70, Tcmb0: 0.}
-	tol = 1e-9
-	z_vec = []float64{1.0, 10.0, 500.0, 1000.0}
+
+	tol := 1e-9
+	z_vec := []float64{1.0, 10.0, 500.0, 1000.0}
 	hubbleDistance := SpeedOfLightKmS / cos.H0
 	for _, z := range z_vec {
 		exp = 2.0 * hubbleDistance * (1 - math.Sqrt(1/(1+z)))
